7bd8eea3fbd028dd4952b5fcfbcf98c9cce0476c
tranhoang3062 <tranhoangnd3062@gmail.com>
2022-05-20 02:39:52 +0700

    done

---
+++ todoList/components/GoalInput.js
@@ -0,0 +1,70 @@
+import { useState } from 'react';
+import { StyleSheet, View, TextInput, Button, Modal, Image} from 'react-native';
+ 
+function GoalInput(props) {
+    const[enteredGoalText, setEnteredGoalText] = useState('');
+    
+    function goalInputHandler(enteredText) {
+        setEnteredGoalText(enteredText);
+    }
+
+    function addGoalHandler() {
+        props.onAddGoal(enteredGoalText);
+        setEnteredGoalText('');
+    }
+    
+    return (
+        <Modal visible={props.visible} animationType="slide">
+            <View style={styles.inputContainer}>
+                <Image style={styles.image} source={require('../assets/images/goal.png')} />
+                <TextInput 
+                    style={styles.textInput}
+                    placeholder='your course goal!' 
+                    onChangeText={goalInputHandler}
+                />
+            <View style={styles.buttonContainer}>
+                <View style={styles.button}>
+                    <Button title='Add Goal' onPress={addGoalHandler}/> 
+                </View>
+                <View style={styles.button}>
+                    <Button title='Cancel' onPress={props.onCancel}/>
+                </View>
+            </View>
+        </View>
+      </Modal>
+    );
+}
+
+export default GoalInput;
+
+const styles = StyleSheet.create({
+    inputContainer: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+        padding: 16,
+        backgroundColor: '#311b6b'
+    },
+    image: {
+        width: 100,
+        height: 100,
+        margin: 20,
+    },
+    textInput: {
+        borderWidth: 1,
+        borderColor: '#e4d0ff',
+        backgroundColor: '#e4d0ff',
+        width: '100%',
+        padding: 8,
+        color: '#234325'
+    },
+    buttonContainer: {
+        flexDirection: 'row',
+        marginTop: 16,
+    },
+    button: {
+        width: 100,
+        marginHorizontal: 8,
+    }
+});
+
